8a7027697661aada03be747637d72f6e25ec1a4e
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-11-11 19:17:07 -0700

    machine

--- Game/memorization-dungeon-crawler/levels/universal/door/door.gd
+++ Game/memorization-dungeon-crawler/levels/universal/door/door.gd
@@ -1,16 +1,17 @@
 extends StaticBody3D
 class_name Door
-var door_open:float = 3
-var door_closed:float = 0
 var open:bool = false
-const slide_speed = 1;
-@onready var door: Node3D = %"door mesh"
+
+@export var door_open_y:float = 3
+@export var door_closed_y:float = 0
+@export var slide_speed = 1;
+@export var door:Node
 
 func setOpen(open2:bool):
 	open = open2;
 
 func _process(delta):
-	if open and door.position.y < door_open:
+	if open and door.position.y < door_open_y:
 		door.position.y = door.position.y + (slide_speed*delta)
-	elif !open and door.position.y > door_closed:
+	elif !open and door.position.y > door_closed_y:
 		door.position.y = door.position.y - (slide_speed*delta)
