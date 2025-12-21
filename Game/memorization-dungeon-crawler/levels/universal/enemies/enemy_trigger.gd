57edbe300fd19c80b70b347d63e984b149bc9342
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-12-21 15:19:14 -0700

    fixed yeti animation

--- Game/memorization-dungeon-crawler/levels/universal/enemies/enemy_trigger.gd
+++ Game/memorization-dungeon-crawler/levels/universal/enemies/enemy_trigger.gd
@@ -70,9 +70,9 @@ func play_idle_animation():
 	#print("Playing idle animation")
 	#We want to check if fight idle animaiton is not null and is not empty
 	if(fighting and fight_idle_animation != null and !fight_idle_animation.strip_edges().is_empty()):
-		_animation_player.play(fight_idle_animation,0.4,fight_idle_animation_speed)
+		_animation_player.play(fight_idle_animation,0.3,fight_idle_animation_speed)
 	else:
-		_animation_player.play(idle_animation,0.4,idle_animation_speed)
+		_animation_player.play(idle_animation,0.3,idle_animation_speed)
 
 func _single_drill(success):
 	if(success):
@@ -141,6 +141,13 @@ func _process(delta):
 		
 		if(is_instance_valid(_animation_player) and !_animation_player.is_playing()):
 			play_idle_animation()
+		elif(_animation_player.current_animation == take_hit_animation):
+			#End our take hit animation early to ensure smooth transition
+			var length = _animation_player.current_animation.length()
+			var time = _animation_player.current_animation_position
+			if time > length - 0.3:#seconds
+				play_idle_animation()
+			
 		var dir = target_pos - self_pos
 		dir.y = 0
 		dir = dir.normalized()
